4ec19b4bfd68d0cd4c0dfe579506164376cb2f1b
Marc <mplanas92@gmail.com>
2022-11-25 21:34:05 +0100

    graphs

--- src/tools/visualization.py
+++ src/tools/visualization.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 from matplotlib.lines import Line2D
 from matplotlib.patches import Patch
+import plotly.express as px
 
 # Seasons:
 # Primavera: inicia el 20 de marzo al 21 de junio.
@@ -25,7 +26,7 @@ def plot_lineplot_timeseries(name,year_min,year_max):
     data=df[(df['Year'] >= year_min) & (df['Year'] < year_max)], 
     ax=ax, color='black', linewidth = 0.5)
     plt.ylim(bottom = 0)
-    
+
     # Color seasons
     for i in range(year_min,year_max):
         ax.axvspan(f'{i}-03-20',f'{i}-06-20', color="green", alpha=0.1) # spring
@@ -33,13 +34,16 @@ def plot_lineplot_timeseries(name,year_min,year_max):
         ax.axvspan(f'{i}-09-23',f'{i}-12-20', color="brown", alpha=0.1) # fall
         ax.axvspan(f'{i}-12-21',f'{i}-12-31', facecolor="blue", alpha=0.1) # winter
         ax.axvspan(f'{i}-01-01',f'{i}-03-19', facecolor="blue", alpha=0.1) # winter
-    plt.xticks(rotation=45)
+    # plt.xticks(rotation=45)
+    plt.title(f"PM10 concentration evolution in {name}", y = 1.02, fontweight="bold", fontsize = 15)
+    plt.ylabel("Concentration of PM10 [µg/m$^3$]")
+    plt.xlabel("Year")
 
     # Add horizontal lines for air quality
     plt.axhline(y = 20, color = 'black', linestyle = 'dotted', alpha = 0.2, label = "Good")
-    plt.axhline(y = 50, color = 'black', linestyle = 'dotted', alpha = 0.3, label = "Fair")
-    plt.axhline(y = 100, color = 'black', linestyle = 'dotted', alpha = 0.4, label = "Poor")
-    plt.axhline(y = 150, color = 'black', linestyle = 'dotted', alpha = 0.5, label = "Very Poor")
+    plt.axhline(y = 50, color = 'black', linestyle = 'dotted', alpha = 0.4, label = "Fair")
+    plt.axhline(y = 100, color = 'black', linestyle = 'dotted', alpha = 0.6, label = "Poor")
+    plt.axhline(y = 150, color = 'black', linestyle = 'dotted', alpha = 0.8, label = "Very Poor")
 
     # Add custom legend for seasons (Patch) and air quality (Line2D)
     legend_elements = [Patch(color='blue', alpha = 0.2, label='Winter'), 
@@ -53,5 +57,36 @@ def plot_lineplot_timeseries(name,year_min,year_max):
                         Line2D([0], [0], color='black', lw=1, label = 'Good',linestyle = 'dotted', alpha = 0.4)
     ]
     ax.legend(handles=legend_elements, bbox_to_anchor=(1.02, 1))
-
+    fig.savefig(f"./output/time_series/{name}.jpg", dpi=1000, bbox_inches='tight')
     return plt.show()
+
+# ------------------------------------------------------------------------------------------------------------
+def plot_map(df, geo_json, match_location, value_plotted, animation, palette, range):
+    fig = px.choropleth_mapbox(data_frame=df,
+                            geojson=geo_json,
+                            locations=df[match_location],
+                            featureidkey = 'properties.name',
+                            color=value_plotted,
+                            center={'lat':58, 'lon':10},
+                            mapbox_style='open-street-map',
+                            zoom=2,
+                            color_continuous_scale=palette,
+                            range_color=(range[0], range[1]),
+                            animation_frame=animation,
+                            width=850,
+                            height=650)
+    return fig
+
+# ------------------------------------------------------------------------------------------------------------
+def plot_lineplot(df, location_subset, x, y, order_legend, title):
+    # Subset
+    location = location_subset
+    dataset = df[df['Location'].isin(location)]
+    # plot
+    fig = sns.lineplot(x=x, y=y, data=dataset, hue='Location', marker='o', palette='Set1');
+    plt.title(title, y = 1.02, fontweight="bold", fontsize = 15);
+    # reordering the labels
+    handles, labels = plt.gca().get_legend_handles_labels()
+    # pass handle & labels lists along with order as below
+    plt.legend([handles[i] for i in order_legend], [labels[i] for i in order_legend])
+    return fig
